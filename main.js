d292d5f76387cad5806ed5572702ba568649b72f
Mary Andrea Doria Gomez <mdoria@bancolombia.com.co>
2023-10-26 00:56:16 -0500

    Correcion a English

--- main.js
+++ main.js
@@ -43,9 +43,9 @@ async function loadPokemonData(pokemonName) {
             <img src="${data.sprites.front_default}" class="card-img-top" alt="${data.name}">
             <div class="card-body">
                 <h5 class="card-title">${data.name.toUpperCase()}</h5>
-                <p class="card-text">Número: ${data.id}</p>
-                <p class="card-text">Altura: ${data.height / 10}m</p>
-                <p class="card-text">Peso: ${data.weight / 10}kg</p>
+                <p class="card-text">Number: ${data.id}</p>
+                <p class="card-text">Height: ${data.height / 10}m</p>
+                <p class="card-text">Weight: ${data.weight / 10}kg</p>
                 <p class="card-text">Description: ${speciesData.flavor_text_entries[0].flavor_text}</p>
                 <p class="card-text">Skills: ${data.abilities.map(ability => ability.ability.name).join(', ')}</p>
                 <p class="card-text">Movements: ${data.moves.slice(0, 5).map(move => move.move.name).join(', ')}</p>
